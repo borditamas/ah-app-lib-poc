c12cb2fb28f4c5a46008df233eae83d5e16b5dee
Tamas Bordi <borditamas@gmail.com>
2022-03-02 16:05:12 +0100

    library structure (not finished)

--- core-mandatory/src/main/java/ai/aitia/arrowhead/application/common/networking/PayloadResolver.java
+++ core-mandatory/src/main/java/ai/aitia/arrowhead/application/common/networking/PayloadResolver.java
@@ -6,29 +6,42 @@ import java.io.ObjectInputStream;
 
 import ai.aitia.arrowhead.application.common.verification.Ensure;
 
-public class PayloadResolver<T> {
+public class PayloadResolver<P> {
 	
-	private T payload;
+	private P payload;
+	private Object fullMessage;
 	
-	public T getPayload() {
+	public P getPayload() {
 		return this.payload;
-	}
+	}	
 	
 	@SuppressWarnings("unchecked")
-	public void read(final Object data) {
-		Ensure.isTrue(this.payload.getClass().isAssignableFrom(data.getClass()), "Data to be loaded is not assignable to " + this.payload.getClass().getSimpleName());
-		this.payload = (T)data;
+	public <F>F getFullMessage(final Class<F> type) {
+		Ensure.isTrue(type.isAssignableFrom(this.fullMessage.getClass()), "Full message is not assignable to " + type.getSimpleName());
+		return (F)this.fullMessage;
 	}
 
 	@SuppressWarnings("unchecked")
-	public void read(final byte[] data) {
+	public void read(final Object data, final Object fullMessage) {
+		Ensure.isTrue(this.payload.getClass().isAssignableFrom(data.getClass()), "Data to be loaded is not assignable to " + this.payload.getClass().getSimpleName());
+		this.payload = (P)data;
+		this.fullMessage = fullMessage;
+	}
+
+	@SuppressWarnings("unchecked")
+	public void read(final byte[] data, final Object fullMessage) {
 		try {
 			final ByteArrayInputStream bis = new ByteArrayInputStream(data);
 			final ObjectInput in = new ObjectInputStream(bis);			
-			this.payload = (T)in.readObject();
+			this.payload = (P)in.readObject();
+			this.fullMessage = fullMessage;
 			
 		} catch (final Exception ex) {
 			Ensure.fail("Data to be loaded is not assignable to " + this.payload.getClass().getSimpleName() + ". " + ex.getMessage());
 		}
+	}
+	
+	public void read(final Object fullMessage) {
+		this.fullMessage = fullMessage;
 	}
 }
