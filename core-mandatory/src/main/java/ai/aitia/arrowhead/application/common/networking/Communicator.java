45550f45b6f0adec5c5e68e321cd70591ed7a763
Tamas Bordi <borditamas@gmail.com>
2022-03-03 14:36:17 +0100

    library structure (not finished)

--- core-mandatory/src/main/java/ai/aitia/arrowhead/application/common/networking/Communicator.java
+++ core-mandatory/src/main/java/ai/aitia/arrowhead/application/common/networking/Communicator.java
@@ -1,11 +1,13 @@
 package ai.aitia.arrowhead.application.common.networking;
 
+import ai.aitia.arrowhead.application.common.networking.decoder.PayloadDecoder;
 import ai.aitia.arrowhead.application.common.networking.profile.InterfaceProfile;
 
 public interface Communicator {
 
 	public CommunicatorType type();
 	public void properties(final CommunicationProperties props);
+	public void decoder(final PayloadDecoder decoder);
 	public void initialize();
 	public boolean isInitialized();
 	public CommunicationClient client(final InterfaceProfile interfaceProfile);
